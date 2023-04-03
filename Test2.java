e7192279eea3d948235742e33c7b354c8b0e6a74
IlTetta <116306022+IlTetta@users.noreply.github.com>
2023-04-03 09:49:54 +0200

    cambio path

--- Test2.java
+++ Test2.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Test2 {
     public static void main(String[] args) throws Exception {
         try {
-            String filePath = "./geonames-and-coordinates.csv";
+            String filePath = "./CoordinateMonitoraggio.dati.csv";
             FileReader fileReader = new FileReader(filePath);
 
             CSVReader openCsvReader = new CSVReader(fileReader);
