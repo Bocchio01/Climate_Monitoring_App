c142c4afd2cb1a527415e72a91e1dd86de953884
IlTetta <116306022+IlTetta@users.noreply.github.com>
2023-04-03 11:12:33 +0200

    modifica operatore

--- Operatore.java
+++ Operatore.java
@@ -4,12 +4,12 @@ import prog.io.*;
 
 public class Operatore {
 
-    String nomeCognome;
-    String codFiscale;
-    String eMail;
-    int userID;
-    String password;
-    String centroMonitoraggio;
+    public String nomeCognome;
+    public String codFiscale;
+    public String eMail;
+    public static int userID = 1;
+    private String password;
+    public String centroMonitoraggio;
 
     public Operatore(String nomeCognome, String codFiscale, String eMail, int userID, String password,
             String centroMonitoraggio) {
@@ -17,13 +17,13 @@ public class Operatore {
         this.nomeCognome = nomeCognome;
         this.codFiscale = codFiscale;
         this.eMail = eMail;
-        this.userID = userID;
+        Operatore.userID = userID++;
         this.password = password;
         this.centroMonitoraggio = centroMonitoraggio;
 
     }
 
-    private String registrazione() {
+    private void registrazione() {
         ConsoleInputManager in = new ConsoleInputManager();
         System.out.println("Inserisci il tuo nome e cognome");
         String nomeCognome = in.readLine();
@@ -31,12 +31,17 @@ public class Operatore {
         String codFiscale = in.readLine();
         System.out.println("Inserisci la tua e-mail");
         String eMail = in.readLine();
-        System.out.println("Inserisci la tua password");
+        System.out.println("Inserisci la tua password lunga almeno 8 caratteri");
         String password = in.readLine();
+        while (password.length() < 7) {
+            System.out.println("Password troppo corta, reinseriscila");
+            password = in.readLine();
+        }
         System.out.println("Inserisci, se vuoi, il tuo centro di Monitoraggio di appartenenza");
         String centroMonitoraggio = in.readLine();
         int userID = codFiscale.charAt(0) + codFiscale.charAt(1) + codFiscale.charAt(2);
-        System.out.println("Il tuo userID e': ");
+        System.out.println("Registrazione completata. Accedi all'App usando questo userID: " + Operatore.userID
+                + " e la tua password");
 
     }
 }
