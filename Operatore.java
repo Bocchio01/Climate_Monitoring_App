7f18baf214b7a1b67654c878ba39efb0bca868a7
IlTetta <116306022+IlTetta@users.noreply.github.com>
2023-04-03 11:00:38 +0200

    operatore modifica

--- Operatore.java
+++ Operatore.java
@@ -1,3 +1,42 @@
+import java.io.Console;
+
+import prog.io.*;
+
 public class Operatore {
 
+    String nomeCognome;
+    String codFiscale;
+    String eMail;
+    int userID;
+    String password;
+    String centroMonitoraggio;
+
+    public Operatore(String nomeCognome, String codFiscale, String eMail, int userID, String password,
+            String centroMonitoraggio) {
+
+        this.nomeCognome = nomeCognome;
+        this.codFiscale = codFiscale;
+        this.eMail = eMail;
+        this.userID = userID;
+        this.password = password;
+        this.centroMonitoraggio = centroMonitoraggio;
+
+    }
+
+    private String registrazione() {
+        ConsoleInputManager in = new ConsoleInputManager();
+        System.out.println("Inserisci il tuo nome e cognome");
+        String nomeCognome = in.readLine();
+        System.out.println("Inserisci il tuo codice fiscale");
+        String codFiscale = in.readLine();
+        System.out.println("Inserisci la tua e-mail");
+        String eMail = in.readLine();
+        System.out.println("Inserisci la tua password");
+        String password = in.readLine();
+        System.out.println("Inserisci, se vuoi, il tuo centro di Monitoraggio di appartenenza");
+        String centroMonitoraggio = in.readLine();
+        int userID = codFiscale.charAt(0) + codFiscale.charAt(1) + codFiscale.charAt(2);
+        System.out.println("Il tuo userID e': ");
+
+    }
 }
