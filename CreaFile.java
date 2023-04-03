a9c1715eb3b52510fccd58dae29303c229e79146
IlTetta <116306022+IlTetta@users.noreply.github.com>
2023-04-03 10:06:48 +0200

    change CreaFile

--- CreaFile.java
+++ CreaFile.java
@@ -1,12 +1,31 @@
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
+import prog.io.*;
 
 public class CreaFile {
   public static void main(String[] args) {
+    ConsoleInputManager in=new ConsoleInputManager();
     try {
-      File file = new File("filename.txt");
-      if (file.createNewFile()) {
-        System.out.println("Il file creato e': " + file.getName());
+
+        
+      File file00 = new File("OperatoriRegistrati.dati.txt");
+      File file01 = new File("CentroMonitoraggio.dati.txt");
+      File file02 = new File("ParametriClimatici.dati.txt");
+      
+      if (file00.createNewFile()) {
+        System.out.println("Il file creato e': " + file00.getName());
+      } else {
+        System.out.println("Il file esiste gia'.");
+      }
+
+      if (file01.createNewFile()) {
+        System.out.println("Il file creato e': " + file01.getName());
+      } else {
+        System.out.println("Il file esiste gia'.");
+      }
+
+      if (file02.createNewFile()) {
+        System.out.println("Il file creato e': " + file02.getName());
       } else {
         System.out.println("Il file esiste gia'.");
       }
