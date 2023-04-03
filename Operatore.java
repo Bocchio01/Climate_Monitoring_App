97cff541d746bc4817b4f80905e42440463e6bc6
IlTetta <116306022+IlTetta@users.noreply.github.com>
2023-04-03 11:26:01 +0200

    modifica operatore

--- Operatore.java
+++ Operatore.java
@@ -1,4 +1,8 @@
+opimport java.io.BufferedWriter;
 import java.io.Console;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import prog.io.*;
 
@@ -23,25 +27,64 @@ public class Operatore {
 
     }
 
-    private void registrazione() {
+    private void registrazione() throws IOException {
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        PrintWriter out = null;
         ConsoleInputManager in = new ConsoleInputManager();
-        System.out.println("Inserisci il tuo nome e cognome");
-        String nomeCognome = in.readLine();
-        System.out.println("Inserisci il tuo codice fiscale");
-        String codFiscale = in.readLine();
-        System.out.println("Inserisci la tua e-mail");
-        String eMail = in.readLine();
-        System.out.println("Inserisci la tua password lunga almeno 8 caratteri");
-        String password = in.readLine();
-        while (password.length() < 7) {
-            System.out.println("Password troppo corta, reinseriscila");
-            password = in.readLine();
+
+        try {
+            fw = new FileWriter("OperatoriRegistrati.dati.txt", true);
+            bw = new BufferedWriter(fw);
+            out = new PrintWriter(bw);
+
+            System.out.println("Inserisci il tuo nome e cognome");
+            String nomeCognome = in.readLine();
+            out.print(nomeCognome + "\t");
+            System.out.println("Inserisci il tuo codice fiscale");
+            String codFiscale = in.readLine();
+            out.print(codFiscale + "\t");
+            System.out.println("Inserisci la tua e-mail");
+            String eMail = in.readLine();
+            out.print(eMail + "\t");
+            System.out.println("Inserisci la tua password lunga almeno 8 caratteri");
+            String password = in.readLine();
+            while (password.length() < 7) {
+                System.out.println("Password troppo corta, reinseriscila");
+                password = in.readLine();
+            }
+            out.print(password);
+
+            System.out.println("Inserisci, se vuoi, il tuo centro di Monitoraggio di appartenenza");
+            String centroMonitoraggio = in.readLine();
+            if (centroMonitoraggio != null)
+                out.print(centroMonitoraggio);
+
+            int userID = codFiscale.charAt(0) + codFiscale.charAt(1) + codFiscale.charAt(2);
+            System.out.println("Registrazione completata. Accedi all'App usando questo userID: " + Operatore.userID
+                    + " e la tua password");
+            out.print(userID);
+
+            out.close();
+
+        } catch (IOException e) {
+            // exception handling left as an exercise for the reader
+        } finally {
+            if (out != null)
+                out.close();
+            try {
+                if (bw != null)
+                    bw.close();
+            } catch (IOException e) {
+                // exception handling left as an exercise for the reader
+            }
+            try {
+                if (fw != null)
+                    fw.close();
+            } catch (IOException e) {
+                // exception handling left as an exercise for the reader
+            }
         }
-        System.out.println("Inserisci, se vuoi, il tuo centro di Monitoraggio di appartenenza");
-        String centroMonitoraggio = in.readLine();
-        int userID = codFiscale.charAt(0) + codFiscale.charAt(1) + codFiscale.charAt(2);
-        System.out.println("Registrazione completata. Accedi all'App usando questo userID: " + Operatore.userID
-                + " e la tua password");
 
     }
 }
