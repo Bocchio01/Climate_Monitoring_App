b269b263132d8c836b5870326240feab0392f492
IlTetta <116306022+IlTetta@users.noreply.github.com>
2023-04-05 18:49:27 +0200

    TODO algoritmi per luca

--- trova_coordinate.java
+++ trova_coordinate.java
@@ -1,43 +1,46 @@
 import java.io.*;
 
 public class trova_coordinate {
-    public static void main(String[] args) 
-    throws IOException {
+    public static void main(String[] args)
+            throws IOException {
         FileReader f;
-        f=new FileReader("./CoordinateMonitoraggi.dati.csv");//"./Dati_abbreviati.csv"
-    
-        BufferedReader b;
-        b=new BufferedReader(f);
-    
-        String s, s_latitudine="45", s_longitudine="70", closest="";
-        String[] riga = new String[7];
-        int j=0, m=0, i=0, l=0;
-        double d_latitude = Double.parseDouble(s_latitudine), d_longitude = Double.parseDouble(s_longitudine);
-        double data_latitude, data_longitude, distance=400, temp_distance;
+        f = new FileReader("./CoordinateMonitoraggi.dati.csv");// "./Dati_abbreviati.csv"
 
-        s=b.readLine()+";";
-        
+        BufferedReader b;
+        b = new BufferedReader(f);
+
+        String s, s_latitudine = "45", s_longitudine = "70", closest = "";
+        String[] riga = new String[7];
+        int j = 0, m = 0, i = 0, l = 0;
+        double d_latitude = Double.parseDouble(s_latitudine), d_longitude = Double.parseDouble(s_longitudine);
+        double data_latitude, data_longitude, distance = 400, temp_distance;
+
+        s = b.readLine() + ";";
+        // TODO Luca prova a vedere se conviene utilizzare il metodo split() per le
+        // TODO stringhe invece del substring, stessa cosa nel trova_stringa
         while (true) {
-            s=b.readLine()+";";
-            l=s.length();
+            s = b.readLine() + ";";
+            l = s.length();
             for (j = 0; j < l; j++) {
-                if(s.charAt(j)==';'){
-                    riga[i] = s.substring(m,j);
-                    m=j+1;
+                if (s.charAt(j) == ';') {
+                    riga[i] = s.substring(m, j);
+                    m = j + 1;
                     i++;
                 }
             }
-            i=0;
-            m=0;
+            i = 0;
+            m = 0;
             data_latitude = Double.parseDouble(riga[5]);
             data_longitude = Double.parseDouble(riga[6]);
-            temp_distance = Math.sqrt(Math.pow(data_latitude-d_latitude, 2)+Math.pow(data_longitude-d_longitude, 2));
-            if(distance > temp_distance) {
+            temp_distance = Math
+                    .sqrt(Math.pow(data_latitude - d_latitude, 2) + Math.pow(data_longitude - d_longitude, 2));
+            if (distance > temp_distance) {
                 distance = temp_distance;
                 closest = s;
             }
-            if(b.readLine()==null) break;
+            if (b.readLine() == null)
+                break;
         }
         System.out.println(closest);
-      }
+    }
 }
