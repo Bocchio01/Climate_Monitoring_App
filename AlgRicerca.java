c564f344b6a22250c563eafabe5eb8964f17cd52
IlTetta <116306022+IlTetta@users.noreply.github.com>
2023-04-09 16:54:27 +0200

    fix

--- AlgRicerca.java
+++ AlgRicerca.java
@@ -3,12 +3,19 @@ import java.io.*;
 import java.util.*;
 import prog.io.ConsoleInputManager;
 import java.text.Normalizer;
+import java.nio.charset.StandardCharsets;
 
 class AlgRicerca {
     public static void main(String[] args) {
+
         ConsoleInputManager in = new ConsoleInputManager();
+        System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.ISO_8859_1));
+        Scanner scanner = new Scanner(new InputStreamReader(System.in, StandardCharsets.ISO_8859_1));
+
         String nomeFile = "CoordinateMonitoraggi.dati.csv";
-        String ricerca = in.readLine("Inserisci il luogo che vuoi visualizzare" + "\n");
+        System.out.println("Inserisci il luogo che vuoi visualizzare" + "\n");
+        String ricerca = scanner.nextLine();
+        ricerca = Normalizer.normalize(ricerca, Normalizer.Form.NFC);
 
         char c;
         int q = 0;
@@ -18,7 +25,7 @@ class AlgRicerca {
             c = ricerca.charAt(i);
             if (!Character.isLetter(c)) {
                 subs = ricerca.substring(q, i);
-                ricerca = ricerca.substring(i + 1);
+                ricerca = ricerca.substring(i);
                 ricerca = subs + ricerca;
             }
         }
