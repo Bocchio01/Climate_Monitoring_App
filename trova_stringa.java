8b4cf5e703658edc2bb04acc720e8ae30f13e339
Masce02 <116431590+Masce02@users.noreply.github.com>
2023-04-20 10:16:36 +0200

    Ciao

--- trova_stringa.java
+++ trova_stringa.java
@@ -1,52 +1,16 @@
 import java.io.*;
-import java.text.Normalizer;
 import prog.io.ConsoleInputManager;
-import java.util.Scanner;
-import java.nio.charset.StandardCharsets;
-import java.nio.charset.*;
-import prog.io.*;
-
 public class trova_stringa {
-    public static void main(String[] args)
-            throws IOException {
 
-        ConsoleInputManager in = new ConsoleInputManager();
-
-        System.setIn(new java.io.BufferedInputStream(System.in));
-        System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.ISO_8859_1));
-
-        Scanner scanner = new Scanner(new InputStreamReader(System.in, StandardCharsets.ISO_8859_1));
-
-    public static void main(String[] args) 
-    throws IOException {
+    public static void main(String[] args) throws IOException {
 
         ConsoleInputManager in = new ConsoleInputManager();
 
         FileReader f;
-        f = new FileReader("./CoordinateMonitoraggi.dati.csv");// "./Dati_abbreviati.csv"
+        f = new FileReader("./geonames-and-coordinates.csv");// "./Dati_abbreviati.csv"
 
         BufferedReader b;
         b = new BufferedReader(f);
-
-        String s, ricerca, subs = "";
-        System.out.print("Inserisci una parola con caratteri accentati: ");
-        ricerca = scanner.nextLine();
-        ricerca = Normalizer.normalize(ricerca, Normalizer.Form.NFC);
-        System.out.println(ricerca);
-
-        char c;
-        int q = 0;
-
-        for (int i = 0; i < ricerca.length(); i++) {
-            c = ricerca.charAt(i);
-            if (!Character.isLetter(c)) {
-                subs = ricerca.substring(q, i);
-                ricerca = ricerca.substring(i);
-                ricerca = subs + ricerca;
-            }
-        }
-        ricerca = ricerca.toLowerCase();
-        System.out.println(ricerca);
 
         b=new BufferedReader(f);
 
@@ -55,16 +19,16 @@ public class trova_stringa {
             System.err.println("No console");
             System.exit(1);
         }
-        String command = console.readLine("Enter command: %n");
-        System.out.format("command: %s%n", command);
-        if (command.equals("Č")) {
+        String ricerca = console.readLine("Enter ricerca: %n");
+        System.out.format("ricerca: %s%n", ricerca);
+        if (ricerca.equals("Č")) {
             System.out.println("Test was successful!");
         }
     
-        String s, ricerca="milan";
+        String s;
         //ricerca = in.readLine("Che città vuoi cercare? ");
         //Reader input = new InputStreamReader(, "UTF-8");
-        command = command.toLowerCase();
+        ricerca = ricerca.toLowerCase();
         String[] riga = new String[7];
         int j=0, m=0, i=0, l=0;
 
@@ -72,7 +36,7 @@ public class trova_stringa {
 
         //ricerca = new String(originalBytes, StandardCharsets.UTF_8);
 
-        System.out.println(command);
+        System.out.println(ricerca);
 
         s = b.readLine() + "\t";
 
@@ -89,7 +53,7 @@ public class trova_stringa {
             i=0;
             m=0;
             for (int n = 0; n < 7; n++) {
-                if (riga[n].equals(command)) {
+                if (riga[n].equals(ricerca)) {
                     System.out.println(s);
                     break;
                 }
