99234eedf42d85f53456860dfef4d1353c5aaf6c
Masce02 <116431590+Masce02@users.noreply.github.com>
2023-04-20 10:08:47 +0200

    FUNZIONAAAAAA

--- trova_stringa.java
+++ trova_stringa.java
@@ -1,18 +1,42 @@
 import java.io.*;
+import java.nio.charset.*;
+import prog.io.*;
 
 public class trova_stringa {
     public static void main(String[] args) 
     throws IOException {
+
+        ConsoleInputManager in = new ConsoleInputManager();
+
         FileReader f;
         f=new FileReader("./CoordinateMonitoraggi.dati.csv");//"./Dati_abbreviati.csv"
     
         BufferedReader b;
         b=new BufferedReader(f);
+
+        Console console = System.console();
+        if (console == null) {
+            System.err.println("No console");
+            System.exit(1);
+        }
+        String command = console.readLine("Enter command: %n");
+        System.out.format("command: %s%n", command);
+        if (command.equals("Č")) {
+            System.out.println("Test was successful!");
+        }
     
-        String s, ricerca="Watson";
-        ricerca = ricerca.toLowerCase();
+        String s, ricerca="milan";
+        //ricerca = in.readLine("Che città vuoi cercare? ");
+        //Reader input = new InputStreamReader(, "UTF-8");
+        command = command.toLowerCase();
         String[] riga = new String[7];
         int j=0, m=0, i=0, l=0;
+
+        //byte[] originalBytes = ricerca.getBytes(StandardCharsets.ISO_8859_1);
+
+        //ricerca = new String(originalBytes, StandardCharsets.UTF_8);
+
+        System.out.println(command);
 
         boolean find=false;
 
@@ -31,7 +55,7 @@ public class trova_stringa {
             i=0;
             m=0;
             for (int n = 0; n < 7; n++) {
-                if (riga[n].equals(ricerca)) {
+                if (riga[n].equals(command)) {
                     System.out.println(s);
                     find=true;
                     break;
