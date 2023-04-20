48a8a97616a841ea8569a10903e9fe97e192f960
MartaPontivi <116351536+MartaPontivi@users.noreply.github.com>
2023-04-20 09:36:09 +0200

    logo

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
         String zona;
 
         out.println("Inserisci 1 se sei un Utente o 2 se sei un Operatore");
-        int scelta = in.readInt();
+        char scelta = in.readChar();
         if (scelta == 1) {
             out.println("Inserisci la zona che vuoi trovare");
             cercaAreaGeografica(zona);// da definire
@@ -17,22 +17,22 @@ public class Main {
         } else if (scelta == 2) {
             out.println("Sei un nuovo operatore?");
             String azione = in.readLine();
-            if (azione == "signup") {
+            if (azione == signup) {
                 registrazione();// da definire
-            } else if (azione == "login") {
+            } else if (azione == login) {
                 autenticazione();// da definire
             }
 
             out.println("Cosa vuole fare?");
             String azione2 = in.readLine();
-            if (azione == "cercaarea") {
+            if (azione == cercaarea) {
                 out.println("Inserisci la zona che vuoi trovare");
                 cercaAreaGeografica(zona);// da definire
                 out.println("Vuoi inserire dei nuovi dati o modificarli?");
                 String scelta2 = in.readLine();
-                if (scelta2 == "si") {
-                    inserisciParametriCLimatici(zona);
-                } else if (scelta2 == "no") {
+                if (scelta2 == si) {
+                    inserisciParametriClimatici(zona);
+                } else if (scelta2 == no) {
                     visulazziAreaGeografica(zona);// da definire
 
                 }
