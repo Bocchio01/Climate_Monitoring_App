3edef0e2bc78b996121e35a93fcae59651eec776
MartaPontivi <116351536+MartaPontivi@users.noreply.github.com>
2023-04-20 13:11:04 +0200

    layout

--- Cittadino.java
+++ Cittadino.java
@@ -1,3 +1,8 @@
+/**
+ * La classe Cittadino serve per gestire i metodi disponibili all'utente
+ * @author Mascetti Luca, Pontivi Marta, Romito Andrea, Tettamanti Andrea 
+ */
+
 public class Cittadino {
 
 }
--- Iscrizione.java
+++ Iscrizione.java
@@ -1,3 +1,8 @@
+/**
+ * La classe Iscrizione serve per gestire l'iscrizione dell'operatore all'App
+ * @author Mascetti Luca, Pontivi Marta, Romito Andrea, Tettamanti Andrea 
+ */
+
 public class Iscrizione {
 
 }
